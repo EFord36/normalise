b145927cc767135d478ae131ce48e0200ce03cd3
EFord36 <ef355@cam.ac.uk>
2016-09-16 18:00:52 +0100

    Updated setup.py
    Changed 'Text-Normalisation' to 'normalise' where appropriate. Also
    hopefully improved 'packages' argument

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ use_setuptools()
 from setuptools import setup
 
 setup(
-    name='Text-Normalisation',
-    packages=['Text-Normalisation'],
+    name='normalise',
+    packages=find_packages(exclude='tests, evaluation'),
     version='1.0',
     description='A module to normalise non-standard words in text',
     author='Elliot Ford, Emma Flint',
     author_email='ef355@cam.ac.uk',
     license="GPL"
-    url='https://github.com/EFord36/Text-Normalisation',
-    download_url='https://github.com/EFord36/Text-Normalisation/tarball/1.0',
+    url='https://github.com/EFord36/normalise',
+    download_url='https://github.com/EFord36/normalise/tarball/1.0',
     keywords=['normalisation', 'text', 'pre-processing'],
     classifiers=[
         "Natural Language :: English",
