0f22a0202f1337d8aa1b1b29455c43e1dfbbae3f
EFord36 <ef355@cam.ac.uk>
2016-09-27 15:21:58 +0100

    Adds support for email
    Closes #87

--- normalise/expand_HTAG.py
+++ normalise/expand_HTAG.py
@@ -5,7 +5,7 @@ from math import log
 from nltk import FreqDist as fd
 from normalise.detect import wordlist, mod_path
 from normalise.expand_NUMB import expand_NUM
-
+from normalise.tagger import has_digit
 with open('{}/data/word_tokenized_lowered.pickle'.format(mod_path), mode='rb') as f:
     word_tokenized_lowered = pickle.load(f)
 
@@ -29,16 +29,49 @@ def expand_HTAG(word):
         return word
 
 
+def email_word(word):
+    if not word:
+        return ''
+    elif word.isdigit():
+        return ' '.join(word)
+    elif word in wordlist:
+        return word
+    elif word in ['co', 'com', 'org']:
+        return word
+    else:
+        if email_dig_pattern.match(word):
+            m = email_dig_pattern.match(word)
+            groups = [m.group(1), m.group(2), m.group(3)]
+            return ' '.join([email_word(group) for group in groups if group])
+        elif len(word) < 4:
+            return ' '.join(word.upper())
+        else:
+            return expand_HTAG(word)
+
+
 def expand_URL(word):
     """Expand tokens tagged URL."""
     try:
+        if '@' in word:
+            out = ''
+            current = ''
+            for c in word:
+                if c == '.':
+                    # out += ' ' + expand_HTAG(current) + ' dot'
+                    out += ' {} dot'.format(current if current in wordlist else email_word(current))
+                    current = ''
+                elif c == '@':
+                    # out += ' ' + expand_HTAG(current) + ' at'
+                    out += ' {} at'.format(current if current in wordlist else email_word(current))
+                    current = ''
+                else:
+                    current += c
+            if current:
+                out += ' ' + current
+            return out
         starts = {"http://": "", "https://": "", "www.": "W W W dot"}
-        # starts = ["http://", "https://", "www."]
-        # starts_exp = ['', '', 'W W W dot']
         ends = {".com": "dot com", ".org": "dot org",
-                ".org.uk": "dot org dot U K", ".co.uk": "dot co dot UK"}
-        # ends = [".com", ".org", ".org.uk", ".co.uk"]
-        # ends_exp = ["dot com", "dot org", "dot org dot U K", "dot co dot U K"]
+                ".org.uk": "dot org dot U K", ".co.uk": "dot co dot U K"}
         m = urlstart_pattern.match(word)
         n = urlend_pattern.match(word)
         exp = ''
@@ -108,16 +141,23 @@ hashtag_pattern = re.compile('''
 ''', re.VERBOSE)
 
 urlstart_pattern = re.compile('''
-(https?://|            #'http' followed by optional 's', then '://' OR
-www\.|)
-(.*)              #'www.'
+(https?://|       # 'http' followed by optional 's', then '://' OR
+www\.)            # 'www.'
+(.*)              # followed by anything
 ''', re.VERBOSE | re.IGNORECASE)
 
 urlend_pattern = re.compile('''
-(.*)                      #any number of characters                      # '.'
-(\.com|                 # 'com' OR
-\.org|
-\.org\.uk|          # 'org' followed optionally by '.uk' OR
-\.co\.uk)               # 'co.uk'
-$                       #end of string
+(.*)              # any number of characters
+(\.com|           # '.com' OR
+\.org|            # '.org' OR
+\.org\.uk|        # '.org.uk' OR
+\.co\.uk)         # '.co.uk'
+$                 # end of string
+''', re.VERBOSE | re.IGNORECASE)
+
+email_dig_pattern = re.compile('''
+([A-Za-z]*)       # 0 or more letters
+([0-9]+)          # 1 or more digits
+([A-Za-z]*)       # 0 or more letters
+$                 # end of string
 ''', re.VERBOSE | re.IGNORECASE)
