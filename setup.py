7e66fb2b415f3914923001309e59736a5bf33a8c
EFord36 <ef355@cam.ac.uk>
2016-09-28 10:49:36 +0100

    Fixed bugs
    Comma syntax error, hadn't imported find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from ez_setup import use_setuptools
 use_setuptools()
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='normalise',
@@ -9,7 +9,7 @@ setup(
     description='A module to normalise non-standard words in text',
     author='Elliot Ford, Emma Flint',
     author_email='ef355@cam.ac.uk',
-    license="GPL"
+    license="GPL",
     url='https://github.com/EFord36/normalise',
     download_url='https://github.com/EFord36/normalise/tarball/1.0',
     keywords=['normalisation', 'text', 'pre-processing'],
